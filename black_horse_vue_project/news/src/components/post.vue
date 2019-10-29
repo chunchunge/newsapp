68ab480a26141e4edc32255cab223f11689700b7
chunchunge <54714485+chunchunge@users.noreply.github.com>
2019-10-29 19:31:55 +0800

    完成单图的页面渲染

--- black_horse_vue_project/news/src/components/post.vue
+++ black_horse_vue_project/news/src/components/post.vue
@@ -1,7 +1,12 @@
 <template>
-  <div>
-      <div>
-          {{post.title}}
+  <div> 
+      <div class="singleImgPost">
+          <div class="left">
+              <div class="title">{{post.title}}</div>
+              <div class="info">{{post.user.nickname}} {{post.comment_length}}跟帖</div>
+          </div>
+          <img :src="post.cover[0].url" class="imgRight" alt="">
+         
       </div>
   </div>
 </template>
@@ -14,6 +19,39 @@ export default {
 }
 </script>
 
-<style>
-
+<style lang="less" scoped>
+    .singleImgPost{
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        padding:2.778vw;
+        border-bottom: 1px solid #eee;
+        .left{
+            flex: 1;
+            height: 20.833vw;
+            display: flex;
+            flex-direction: column;
+            justify-content: space-between;
+            padding-right: 2.778vw;
+             .title {
+                display: -webkit-box;
+                -webkit-box-orient: vertical;
+                -webkit-line-clamp: 2;
+                overflow: hidden;
+            }
+             .info {
+                display: -webkit-box;
+                -webkit-box-orient: vertical;
+                -webkit-line-clamp: 1;
+                overflow: hidden;
+                font-size: 14px;
+                color: #888;
+            }
+        }
+        .imgRight{
+            width: 33.611vw;
+            height: 20.833vw;
+            object-fit: cover;
+        }
+    }
 </style>
