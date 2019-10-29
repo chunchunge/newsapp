28da8e4cb4edd81ad9d98f459cbed9359c35dd61
chunchunge <54714485+chunchunge@users.noreply.github.com>
2019-10-29 19:06:43 +0800

    获取默认页面的数据

--- black_horse_vue_project/news/src/pages/Home.vue
+++ black_horse_vue_project/news/src/pages/Home.vue
@@ -1,47 +1,71 @@
 <template>
   <div>
-      <homes/>
-      <van-tabs v-model="activeTab">
-        <van-tab
-        v-for="(tabItem,index) in tabList" :key="index"
+    <homes />
+    <van-tabs v-model="activeTab">
+      <van-tab
+        v-for="(tabItem,index) in tabList"
+        :key="index"
         :title="tabItem.name">
-          内容 {{tabItem.name}}
-        </van-tab>
-      </van-tabs>
+        <div v-for="(post,index) in tabItem.posts" :key="index">
+          {{post.title}}
+        </div>
+      </van-tab>
+    </van-tabs>
   </div>
 </template>
 
 <script>
-import homes from "../components/homes"
+import homes from "../components/homes";
 export default {
-  components:{
+  components: {
     homes
   },
-  data(){
-    return{
-      activeTab:localStorage.getItem('token')?1:0,
-      tabList:[]
-    }
+  data() {
+    return {
+      activeTab: localStorage.getItem("token") ? 1 : 0,
+      tabList: []
+    };
   },
   // 页面加载结束后发送ajax请求
-  mounted(){
-   this.initTabList();
+  mounted() {
+    this.initTabList();
   },
-  methods:{
-    initTabList(){
-       this.$axios({
-      url:'/category',
-      method:'get'
-    }).then(res=>{
-      // 解构data
-      const {data}=res.data;
-      this.tabList=data;
-    })
+  methods: {
+    initTabList() {
+      this.$axios({
+        url: "/category",
+        method: "get"
+      }).then(res => {
+        // 解构data
+        const { data } = res.data;
+        // 分别给每个tab的data加上一个数组来放相对页面的数据
+        data.forEach(element => {
+          element.posts = [];
+        });
+        this.tabList = data;
+        // 获取完了 tab 数据, 马上使用默认 tab (头条) 获取文章列表数据
+        this.getTabPosts(this.activeTab);
+      });
+    },
+    getTabPosts(tabIndex) {
+      // 通过id获取相应数据
+      const categoryId = this.tabList[tabIndex].id;
+      // 发送ajax请求
+      this.$axios({
+        url: "/post",
+        method: "get",
+        // 如果是 get 请求可以使用 params 的属性来带参数
+        params: {
+          category: categoryId
+        }
+      }).then(res => {
+        const { data } = res.data;
+        this.tabList[tabIndex].posts = data;
+      });
     }
   }
-  }
+};
 </script>
 
 <style>
-
 </style>
