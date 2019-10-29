5ca26ffbb0fbd5f28e7bb7b2204e5ae829a5fea2
chunchunge <54714485+chunchunge@users.noreply.github.com>
2019-10-29 17:46:12 +0800

    设置不管登没登陆都显示在头条上

--- black_horse_vue_project/news/src/pages/Home.vue
+++ black_horse_vue_project/news/src/pages/Home.vue
@@ -1,6 +1,13 @@
 <template>
   <div>
       <homes/>
+      <van-tabs v-model="activeTab">
+        <van-tab
+        v-for="(tabItem,index) in tabList" :key="index"
+        :title="tabItem.name">
+          内容 {{tabItem.name}}
+        </van-tab>
+      </van-tabs>
   </div>
 </template>
 
@@ -9,8 +16,30 @@ import homes from "../components/homes"
 export default {
   components:{
     homes
+  },
+  data(){
+    return{
+      activeTab:localStorage.getItem('token')?1:0,
+      tabList:[]
+    }
+  },
+  // 页面加载结束后发送ajax请求
+  mounted(){
+   this.initTabList();
+  },
+  methods:{
+    initTabList(){
+       this.$axios({
+      url:'/category',
+      method:'get'
+    }).then(res=>{
+      // 解构data
+      const {data}=res.data;
+      this.tabList=data;
+    })
+    }
   }
-}
+  }
 </script>
 
 <style>
