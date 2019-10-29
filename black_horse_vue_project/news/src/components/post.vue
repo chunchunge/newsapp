2ee32ee5509ae887898e7b61fde67184c324fed4
chunchunge <54714485+chunchunge@users.noreply.github.com>
2019-10-29 20:02:58 +0800

    完成视频的渲染

--- black_horse_vue_project/news/src/components/post.vue
+++ black_horse_vue_project/news/src/components/post.vue
@@ -1,13 +1,23 @@
 <template>
   <div>
-    <div class="singleImgPost" v-if="post.cover.length>0&&post.cover.length<3">
+      <div class="videoPost" v-if="post.type==2">
+        <div class="title">{{post.title}}</div>
+        <div class="cover">
+         <img :src="post.cover[0].url" class="coverImg" alt />
+        <div class="btnWrapper">
+            <div class="iconfont iconshipin"></div>
+        </div>
+        </div>
+         <div class="info">{{post.user.nickname}} {{post.comment_length}}跟帖</div>
+      </div>
+    <div class="singleImgPost" v-else-if="post.cover.length>0&&post.cover.length<3">
       <div class="left">
         <div class="title">{{post.title}}</div>
         <div class="info">{{post.user.nickname}} {{post.comment_length}}跟帖</div>
       </div>
       <img :src="post.cover[0].url" class="imgRight" alt />
     </div>
-    <div class="multipImgPost" v-if="post.cover.length>=3">
+    <div class="multipImgPost" v-else-if="post.cover.length>=3">
       <div class="title">{{post.title}}</div>
       <div class="covers">
         <img :src="post.cover[0].url" class="coverImg" alt />
@@ -80,6 +90,53 @@ export default {
         }
     }
      .info {
+      display: -webkit-box;
+      -webkit-box-orient: vertical;
+      -webkit-line-clamp: 1;
+      overflow: hidden;
+      font-size: 14px;
+      color: #888;
+    }
+}
+.videoPost{
+    padding:2.778vw;
+    border-bottom: 1px solid #eee;
+.title {
+      display: -webkit-box;
+      -webkit-box-orient: vertical;
+      -webkit-line-clamp: 2;
+      overflow: hidden;
+    }
+    .cover{
+        margin:2.778vw 0;
+        position: relative;
+        .coverImg{
+            width: 100%;
+            height: 47.222vw;
+            object-fit: cover;
+        }
+        .btnWrapper{
+            position: absolute;
+            top:0;
+            left:0;
+            width: 100%;
+            height: 100%;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            .iconfont{
+                height: 15.278vw;
+                line-height: 15.278vw;
+                width:15.278vw;
+                border-radius: 50%;
+                background: #ccc;
+                text-align: center;
+                font-size: 12.778vw;
+                color: #fff;
+            }
+        }
+    }
+    .info {
       display: -webkit-box;
       -webkit-box-orient: vertical;
       -webkit-line-clamp: 1;
