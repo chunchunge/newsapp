8065124f9acefc5133de2720131d66533d7e5800
chunchunge <54714485+chunchunge@users.noreply.github.com>
2019-11-01 21:15:45 +0800

    利用组件实现了搜索

--- black_horse_vue_project/news/src/pages/Search.vue
+++ black_horse_vue_project/news/src/pages/Search.vue
@@ -1,19 +1,43 @@
 <template>
   <div>
       <div class="hearder">
-        <span class="iconfont iconjiantou2"></span>
+        <span class="iconfont iconjiantou2" @click="$router.back()"></span>
         <div class="middle">
           <span class="iconfont iconsearch"></span>
-          <input type="text" class="searchText" placeholder="请输入想要搜索的内容">
+          <input type="text" v-model="searchText" class="searchText" placeholder="请输入想要搜索的内容">
         </div>
-        <div class="btn">搜索</div>
+        <div class="btn" @click="search">搜索</div>
       </div>
+      <post :post='item' v-for="(item, index) in resultList" :key="index"/>
   </div>
 </template>
 
 <script>
+import post from '../components/post'
 export default {
-
+  components: {
+        post
+    },
+  data(){
+    return{
+      searchText:'',
+      resultList: []
+    }
+  },
+  methods:{
+    search(){
+      this.$axios({
+        url:'/post_search',
+        method:'get',
+        params:{
+          keyword:this.searchText
+        }
+      }).then(res=>{
+        const {data}=res.data;
+        this.resultList=data;
+      })
+    }
+  }
 }
 </script>
 
